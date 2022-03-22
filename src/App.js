99bdc911c7c212505e3516eb97e2ecf864b41248
Robyn Arnecke <robynarnecke@gmail.com>
2022-03-22 14:16:43 -0500

    Rudimentary components included and added to App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Navigation from './components/Navigation';
+import Project from './components/Project';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Navigation />
+      <Project src="PokemonVsNature"/>
+      <Project src="TakeMeToYourLeader"/>
+      <Project src="CuriosityCabinet"/>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Project.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Project(props) {
+
+
+    return(
+        <div>
+            {props.src}
+            Project
+        </div>
+    );
+}
