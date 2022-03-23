1851a6a9526cfce7bd46e7f4ce1f87a51223c6e5
Robyn Arnecke <robynarnecke@gmail.com>
2022-03-22 20:03:42 -0500

    Embedded a PDF of my resume in the resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import ResumePDF from '../../assets/Arnecke-Resume.pdf';
 
 export default function Resume() {
   return (
     <div id="Resume">
         <h2>Resume</h2>
+
+        <embed src={ResumePDF} 
+        width="500" 
+        height="375" 
+        type="application/pdf" />
+
     </div>
   );
 }
